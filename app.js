ce1243594f3951b3e03bcfbca90a3eac03d9226c
OlivierNdeye <oliviersndeye@gmail.com>
2024-10-23 21:55:19 -0300

    firt commit

---
+++ app.js
@@ -0,0 +1,83 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    validarDados(){
+        for(let i in this) { //o método for in recuperar as chaves de um determinado objeto, e é colocado dentro de uma variável//o método for in recuperar as chaves de um determinado objeto, e é colocado dentro de uma variável
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false;
+            } 
+        } 
+        return true;
+    }
+}
+
+
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+        if(id === null) {
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id'); //getItem faz a recuperação de um dado no Localstorage
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(d) {
+        let id = this.getProximoId();
+
+        localStorage.setItem(id, JSON.stringify(d));
+
+        localStorage.setItem('id', id);
+    }
+}
+
+let bd = new Bd();
+
+
+function cadastrarDespesa() {
+    
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+    let tipo = document.getElementById('tipo');
+    let descricao = document.getElementById('descricao');
+    let valor = document.getElementById('valor');
+    
+    let despesa = new Despesa(
+        ano.value, 
+        mes.value, 
+        dia.value, 
+        tipo.value, 
+        descricao.value, 
+        valor.value
+    );
+
+
+    if(despesa.validarDados()) {
+        bd.gravar(despesa);
+        //dialog de sucesso
+        $('#sucessoGravacao').modal('show')
+    } else {
+        //dialog de erro
+        $('#erroGravacao').modal('show')
+    }    
+}
+
+
+//acionar o modal de erro de gravação
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+
---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function cadastrarDespesa() {
+    let nome = document.getElementById('nome_paciente');
+    let nasc = document.getElementById('nascimento');
+    let sexo = document.getElementById('sexo');
+    let especialidade = document.getElementById('especialidade');
+
+    console.log(nome.value);
+    console.log(nascimento.value);
+    console.log(sexo.value);
+    console.log(especialidade.value);
+}
